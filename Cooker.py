266dd22827e00c380baa6ad354122ca949f8bf99
Bert09 <61330305+Bert09@users.noreply.github.com>
2020-02-22 09:57:03 +0100

    Create Cooker.py

---
+++ Cooker.py
@@ -0,0 +1,9 @@
+import time
+sleeptime = 1
+print("Lets cook something!")
+food = input("What you want to cook?")
+print("Ok, lets cook " + food + "!")
+print("")
+time.sleep(sleeptime)
+print(food + " finally cooked!")
+delay = input("Press ENTER to exit...")
